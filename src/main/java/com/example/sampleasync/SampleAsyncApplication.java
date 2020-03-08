48a09a73b303028037798ed610cab916f473e179
thankkingdom <thankkingdom@gmail.com>
2020-03-08 22:17:10 +0900

    commit files made by spring initializr

---
+++ src/main/java/com/example/sampleasync/SampleAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleasync/SampleAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
